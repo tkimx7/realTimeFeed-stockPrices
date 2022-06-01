5e86afb38aa46aa59dbf78767e94f02608908c5d
Ty <55491619+tkimx7@users.noreply.github.com>
2022-06-01 09:53:23 -0400

    Update README.R

--- README.R
+++ README.R
@@ -5,6 +5,7 @@
 ### --- Here I publish codes that provide a glimpse into my automated trading strategy. My bot places daily 
 ### --- trades on TDAmeritrade via the "rameritrade" package, an API interface for R. The code in this 
 ### --- repository should help you get started with developing your own real-time feed for prices of stocks 
-### --- and other financial instruments, e.g. options, of your interest. Please refer to the "rameritrade" 
-### --- manual on CRAN for authentication and generating your own access token. Please note that only 120
-### --- API calls are permitted per minute. 
+### --- and other financial instruments, e.g. options, of your interest. The live feed is updated every
+### --- every second on R, and every minute by email should there be large price movements. Please refer to 
+### --- the "rameritrade" manual on CRAN for authentication and generating your own access token. Please note 
+### --- that only 120 API calls are permitted per minute. 
