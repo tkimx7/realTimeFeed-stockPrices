62577cbcd08007340a2a433e0217cefb88be32bd
Ty <55491619+tkimx7@users.noreply.github.com>
2022-06-01 09:57:53 -0400

    Update README.R

--- README.R
+++ README.R
@@ -8,4 +8,6 @@
 ### --- and other financial instruments, e.g. options, of your interest. The live feed is updated every
 ### --- every second on R, and every minute by email should there be large price movements. Please refer to 
 ### --- the "rameritrade" manual on CRAN for authentication and generating your own access token. Please note 
-### --- that only 120 API calls are permitted per minute. 
+### --- that only 120 API calls are permitted per minute. Tokens can be refreshed after 0.5 hours. Repetitive
+### --- refreshing before 0.5 hours is up can result in your access getting banned. The tryCatch error script
+### --- however will avoid this from happening.
