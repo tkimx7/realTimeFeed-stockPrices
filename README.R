c0988208f510bd47bdf86e60e1e485f88ceb6148
Ty <55491619+tkimx7@users.noreply.github.com>
2022-06-02 04:40:37 -0400

    Update README.R

--- README.R
+++ README.R
@@ -6,8 +6,8 @@
 ### --- trades on TDAmeritrade via the "rameritrade" package, an API interface for R. The code in this 
 ### --- repository should help you get started with developing your own real-time feed for prices of stocks 
 ### --- and other financial instruments, e.g. options, of your interest. The live feed is updated every
-### --- every second on R, and every minute by email should there be large price movements. Please refer to 
+### --- every second on R, and every minute by email should there be large price anomalies. Please refer to 
 ### --- the "rameritrade" manual on CRAN for authentication and generating your own access token. Please note 
 ### --- that only 120 API calls are permitted per minute. Tokens can be refreshed after 0.5 hours. Repetitive
-### --- refreshing before 0.5 hours is up can result in your access getting banned. The tryCatch error script
-### --- however will avoid this from happening.
+### --- refreshing before the 0.5 hours is up can result in your access getting banned. The tryCatch error 
+### --- script however should avoid this from happening.
